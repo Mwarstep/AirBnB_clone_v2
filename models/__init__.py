a737d4512c1acb3f2c466943a1be6bce564e1458
Arnold Mwarstep <111042323+Mwarstep@users.noreply.github.com>
2023-03-21 16:16:39 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-import os
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
 
-"""CNC - dictionary = { Class Name (string) : Class Type }"""
-
-if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine import db_storage
-    CNC = db_storage.DBStorage.CNC
-    storage = db_storage.DBStorage()
+if getenv("HBNB_TYPE_STORAGE") == 'db':
+    storage = DBStorage()
 else:
-    from models.engine import file_storage
-    CNC = file_storage.FileStorage.CNC
-    storage = file_storage.FileStorage()
-
+    storage = FileStorage()
 storage.reload()
