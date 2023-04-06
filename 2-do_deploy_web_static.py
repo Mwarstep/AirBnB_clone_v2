67b4b411cfb606c6fb67bf484f6e10dce185bd54
Mwarstep <mvoi811@gmail.com>
2023-04-06 02:18:50 -0700

    Test v.2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,23 +8,28 @@ env.hosts = ["54.160.101.122", "54.237.51.149"]
 
 def do_deploy(archive_path):
     """
-    Will distribute an archve to my web servers
+    Will distribute an archive to my web servers
     """
-
-    if path.exists(archive_path):
-        archive = archive_path.split('/')[1]
-        a_path = "/tmp/{}".format(archive)
-        folder = archive.split('.')[0]
-        fold_path = "/data/web_static/releases/{}/".format(folder)
-
-        put(archive_path, a_path)
-        run("mkdir -p {}".format(fold_path))
-        run("tar -xzf {} -C {}".format(a_path, fold_path))
-        run("rm {}".format(a_path))
-        run("mv -f {}web_static/* {}".format(fold_path, fold_path))
-        run("rm -rf {}web_static".format(fold_path))
+    
+    if not path.exists(archive_path):
+        return False
+    try:
+        tgz_file = archive_path.split("/")[-1]
+        print(tgz_file)
+        filename = tgz_file.split(".")[0]
+        print(filename)
+        pathname = "/data/web_static/releases/" + filename
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(tgz_file, filename))
+        run("rm /tmp/{}".format(tgz_file))
+        run("mv /data/web_static/releases/{}/web_static/* "
+           " /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(fold_path))
-
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
         return True
-    return False
+    except Exception as e:
+        return False
