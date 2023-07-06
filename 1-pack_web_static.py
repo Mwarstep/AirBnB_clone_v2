edaeaa0ed0f05ae0ff1110ec8bf57f3351374ad1
Mwarstep <mvoi811@gmail.com>
2023-07-06 02:32:42 -0700

    File Changes

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 from fabric.api import local
-from datetime import date
 from time import strftime
-
+from datetime import date
 
 def do_pack():
     """
@@ -10,13 +9,13 @@ def do_pack():
     folder of my repo (this one)
     """
 
-    f_name = strftime('%Y%m%d%H%M%S')
+    filename = strftime('%Y%m%d%H%M%S')
     try:
         local("mkdir -p versions")
 
         local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(f_name))
+              .format(filename))
 
-        return "versions/web_static_{}.tgz".format(f_name)
+        return "versions/web_static_{}.tgz".format(filename)
     except Exception as e:
         return None
